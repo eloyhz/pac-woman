5a09a16a6c56f52774c8d24c541f4246cfa2a389
eloyhz <eloyhz@gmail.com>
2017-12-02 19:01:47 -0600

    System module examples

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <SFML/System/Time.hpp>
 #include <SFML/System/Clock.hpp>
+#include <SFML/System/Vector2.hpp>
+#include <SFML/System/Vector3.hpp>
+#include <SFML/System/String.hpp>
 
 int main()
 {
@@ -20,6 +23,30 @@ int main()
     std:: cout << timeInSeconds << std::endl;
     std:: cout << timeInMilliSeconds << std::endl;
     std:: cout << timeInMicroSeconds << std::endl;
+
+    // Represent a 2d-position
+    sf::Vector2<int> vector;
+
+    vector.x = 4;
+    vector.y = 2;
+
+    vector += sf::Vector2<int>(3, 6); // vector addition
+    std::cout << vector.x << ", " << vector.y << std::endl;
+
+    vector *= -1;  // scalar multiplication
+    std::cout << vector.x << ", " << vector.y << std::endl;
+    
+    // vector = direction + magnitude
+
+    sf::Vector2i intVector;
+    sf::Vector2u uintVector;
+    sf::Vector2f floatVector;
+
+    // a 3d-vector
+    sf::Vector3<int> vector3;
+    vector3.z = 8;
+
+    sf::String string = L"Приве́т";
 
     return EXIT_SUCCESS;
 }
