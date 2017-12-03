a62475aca85d1333ad7930258e9d63273bdd7c97
eloyhz <eloyhz@gmail.com>
2017-12-02 21:01:16 -0600

    Graphics

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,68 @@
-#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
-
-void setBestFullScreenMode(sf::Window& window);
-void setDefaultScreenMode(sf::Window& window, int width, int height);
-void changeVideoMode(sf::Window& window);
+/*
+- Drawing shapes
+- Drawing images
+- Drawing text
+- Applying transformations
+*/
 
 int main()
 {
-    sf::Window window;
-    int cont = 0;
+    sf::RenderWindow window(sf::VideoMode(640, 480), "Building Games with SFML");
 
-    changeVideoMode(window);
+    sf::RectangleShape rectangle;
+    rectangle.setSize(sf::Vector2<float>(200,100));
+    rectangle.setFillColor(sf::Color::Green);
+    rectangle.setOutlineThickness(2);
+    rectangle.setOutlineColor(sf::Color::Red);
+
+    sf::CircleShape circle;
+    circle.setRadius(20);
+    circle.setFillColor(sf::Color::Blue);
+    circle.setOutlineThickness(4);
+    circle.setOutlineColor(sf::Color::Yellow);
+
+    sf::ConvexShape customShape;
+    customShape.setPointCount(3);
+    customShape.setPoint(0, sf::Vector2<float>(0, 0));
+    customShape.setPoint(1, sf::Vector2<float>(150, 30));
+    customShape.setPoint(2, sf::Vector2<float>(0, 60));
+    customShape.setFillColor(sf::Color::Magenta);
+
+    sf::Image image;
+    // transparency https://goo.gl/r3eYuc
+    image.loadFromFile("../assets/smiley.png");
+
+    sf::Texture texture;
+    texture.loadFromImage(image);
+
+    sf::Sprite sprite(texture);
+    sf::Sprite sprite2(texture);
+
+    sf::Font font;
+    // downloaded from dafont.com
+    font.loadFromFile("../assets/font.ttf");
+
+    sf::Text text;
+    text.setFont(font);
+    text.setString("Hello-SFML");
+    //text.setColor(sf::Color::Red);
+    text.setPosition(200, 150);
+    text.setCharacterSize(62);
+
+    sprite.setPosition(10,10);
+    sprite.setScale(sf::Vector2<float>(.5,.5));
+
+    sprite2.setPosition(300, 300);
+    sprite2.setRotation(45);
+    sprite2.setScale(.5f, .5f);
+
+    rectangle.setPosition(10, 10);
+    circle.setPosition(400, 50);
+    customShape.setPosition(250, 250);
+
 
     while (window.isOpen())  // ome iteration of the loop is called a tick or frame
     {
@@ -20,64 +71,18 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
-            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
-                window.close();
-            if (event.type == sf::Event::MouseButtonPressed) {
-                std::cout << event.mouseButton.x << ", " << event.mouseButton.y << std::endl;
-                if (event.mouseButton.button == sf::Mouse::Left) 
-                    std::cout << "Left button" << std::endl;
-                else if (event.mouseButton.button == sf::Mouse::Right)
-                    std::cout << "Right button" << std::endl;
-            }
         }
-        // draw here...
+        window.clear();
+        window.draw(rectangle);
+        window.draw(circle);
+        window.draw(customShape);
+        window.draw(sprite);
+        window.draw(sprite2);
+        window.draw(text);
         window.display();
+
         window.setFramerateLimit(60);
         sf::sleep(sf::seconds(1.f/60.f));
     }
     return EXIT_SUCCESS;
-}
-
-
-void fullscreenModes()
-{
-    auto fullscreenModes = sf::VideoMode::getFullscreenModes();
-
-    for (auto & videoMode : fullscreenModes) {
-        std::cout << "Size: " << videoMode.width 
-                    << " & " << videoMode.height
-                    << " bpp: " << videoMode.bitsPerPixel
-                    << std::endl;
-    }
-}
-
-void desktopMode()
-{
-    auto desktopMode = sf::VideoMode::getDesktopMode();
-    std::cout << desktopMode.width << std::endl;
-    std::cout << desktopMode.height << std::endl;
-    std::cout << desktopMode.bitsPerPixel << std::endl;
-}
-
-void changeVideoMode(sf::Window& window)
-{
-    sf::VideoMode videoMode;
-    videoMode.width = 640;
-    videoMode.height = 480;
-    videoMode.bitsPerPixel = 32;
-    window.create(videoMode, "Building games with SFML");
-}  
-
-void setBestFullScreenMode(sf::Window& window)
-{
-    int style = sf::Style::Fullscreen;
-    window.create(sf::VideoMode::getFullscreenModes().front(), "Pac-Woman", style);
-}
-
-void setDefaultScreenMode(sf::Window& window, int width, int height)
-{
-    int style = sf::Style::Close 
-                | sf::Style::Titlebar
-                | sf::Style::Resize; 
-    window.create(sf::VideoMode(width, height), "Pac-Woman");
 }
